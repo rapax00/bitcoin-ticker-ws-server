cd341f1800df1f45341d7316a3d8db71cfdde77e
Rapax <gasparpelayo44@gmail.com>
2024-03-20 23:58:45 -0300

    Minor tiping changes

--- src/service/mempool.js
+++ src/service/mempool.js
@@ -1,18 +1,17 @@
 const mempoolJS = require("@mempool/mempool.js");
 
-async function getLatestBlock() {
+async function getLastBlock() {
     try {
         const { bitcoin } = mempoolJS();
         const latestBlocks = await bitcoin.blocks.getBlocks({
             start_height: undefined,
             end_height: undefined,
         });
-        console.log("Latest block:", latestBlocks[0].height);
-        return latestBlocks[0].height;
+        return latestBlocks[0].height.toString();
     } catch (error) {
         console.error("Error fetching latest block:", error);
         return null;
     }
 }
 
-module.exports = { getLatestBlock };
+module.exports = { getLastBlock };
